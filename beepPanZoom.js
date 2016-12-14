79b001ae1e89eedfafca952dda5e74d87a353c5d
redocnib <beep.exe@gmail.com>
2016-12-15 02:26:12 +0530

    Added Support for Responsive Image Maps
    This will keep image <map> mappings in correct coordinates while zooming.

--- beepPanZoom.js
+++ beepPanZoom.js
@@ -18,6 +18,7 @@ var BeepPanZoom = {
       this.scale = null;
       this.lastScale = null;
       this.img = this.$elem.find("img")[0];
+	  
       this.container = this.img.parentElement;
       this.x = 0;
       this.lastX = 0;
@@ -31,16 +32,83 @@ var BeepPanZoom = {
   },
   options: {
     MIN_SCALE:1,
-    MAX_SCALE:64
+    MAX_SCALE:64,
+	onZoom:null
   },
   
+/* THE BELOW FUNCTION USES A PART OF
+* rwdImageMaps jQuery plugin v1.6
+*
+* Allows image maps to be used in a responsive design by recalculating the area coordinates to match the actual image size on load and window.resize
+*
+* Copyright (c) 2016 Matt Stow
+* https://github.com/stowball/jQuery-rwdImageMaps
+* http://mattstow.com
+* Licensed under the MIT license
+*/
+ rwdImageMap : function() {
+			
+				if (typeof($(this.img).attr('usemap')) == 'undefined')
+					return;
+
+				var that = this.img,
+					$that = $(that);
+
+				// Since WebKit doesn't know the height until after the image has loaded, perform everything in an onload copy
+				$('<img />').on('load', function() {
+					var attrW = 'width',
+						attrH = 'height',
+						w = $that.attr(attrW),
+						h = $that.attr(attrH);
+
+					if (!w || !h) {
+						var temp = new Image();
+						temp.src = $that.attr('src');
+						if (!w)
+							w = temp.width;
+						if (!h)
+							h = temp.height;
+					}
+
+					var wPercent = $that.width()/100,
+						hPercent = $that.height()/100,
+						map = $that.attr('usemap').replace('#', ''),
+						c = 'coords';
+
+					$('map[name="' + map + '"]').find('area').each(function() {
+						var $this = $(this);
+						if (!$this.data(c))
+							$this.data(c, $this.attr(c));
+
+						var coords = $this.data(c).split(','),
+							coordsPercent = new Array(coords.length);
+
+						for (var i = 0; i < coordsPercent.length; ++i) {
+							if (i % 2 === 0)
+								coordsPercent[i] = parseInt(((coords[i]/w)*100)*wPercent);
+							else
+								coordsPercent[i] = parseInt(((coords[i]/h)*100)*hPercent);
+						}
+						$this.attr(c, coordsPercent.toString());
+					});
+				}).attr('src', $that.attr('src'));
+			
+		},
+  
+/* 
+*  END OF rwdImageMaps jQuery plugin v1.6
+*/
+  
+  
   disableImgEventHandlers : function () {
+
        var self=this;
         var events = ['onclick', 'onmousedown', 'onmousemove', 'onmouseout', 'onmouseover',
-                      'onmouseup', 'ondblclick', 'onfocus', 'onblur'];
+                      'onmouseup', 'ondblclick', 'onfocus', 'onblur','contextmenu'];
 
         events.forEach(function (event) {
           self.img[event] = function () {
+
             return false;
           };
         });
@@ -103,7 +171,10 @@ var BeepPanZoom = {
 
         this.img.style.width = Math.ceil(this.curWidth) + 'px';
         this.img.style.height = Math.ceil(this.curHeight) + 'px';
-
+        //if(this.) onZoom
+        
+        if(this.options.onZoom!=null)this.options.onZoom(Math.ceil(this.curWidth),Math.ceil(this.curHeight));
+        this.rwdImageMap();
         // Adjust margins to make sure that we aren't out of bounds
         this.translate(0, 0);
       },
@@ -166,7 +237,7 @@ var BeepPanZoom = {
       }
       ,
       initHammer: function(){
-    
+        this.rwdImageMap();
         var self=this;
         this.disableImgEventHandlers();
         this.imgWidth = this.img.width;
